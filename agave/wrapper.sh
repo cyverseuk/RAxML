45f233e51ce6278a58ff9fe5903bdb16b5afe029
erikvdbergh@gmail.com <erikvdbergh@gmail.com>
2016-11-08 17:07:12 +0000

    add demo data and update app argument options

--- agave/wrapper.sh
+++ agave/wrapper.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cp lib/templatesubmit.htc lib/condorsubmit.htc
-echo arguments = ${xml_file} >> lib/condorsubmit.htc
-echo transfer_input_files = ${xml_file} >> lib/condorsubmit.htc
+echo arguments = ${phy_file} ${model} >> lib/condorsubmit.htc
+echo transfer_input_files = ${phy_file} >> lib/condorsubmit.htc
 echo queue >> lib/condorsubmit.htc
 
 jobid=`condor_submit lib/condorsubmit.htc`
